f7ecb676310ebfe48057248aa082e303ee624b59
pawaninfinite <pawaninfinite@gmail.com>
2021-11-13 14:16:48 +0530

    Update EmployeeRepo.java

--- demo/src/main/java/com/example/demo/repository/EmployeeRepo.java
+++ demo/src/main/java/com/example/demo/repository/EmployeeRepo.java
@@ -3,9 +3,11 @@ package com.example.demo.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Employee;
 
+@Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
 	void deleteEmployeeById(Long id);
